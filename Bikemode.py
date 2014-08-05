7d5c8470af03f7454709bd89b61ff5cca03bb304
Ed Rodriguez <edrodriguezrobles@gmail.com>
2014-08-04 20:59:02 -0500

    bike inustry

---
+++ Bikemode.py
@@ -0,0 +1,39 @@
+class bikeindustry(object):
+
+
+
+
+class wheels():
+	
+	def __init__(self,weight,cost,name):  
+		self.weight = weight
+		self.cost = cost
+		self.name = name
+	
+
+type_1 = wheels(5,2.50,"disc break")   # Types of Wheels
+type_2 = wheels(4,2.25,"rim brakes")
+type_3 = wheels(4.50, 3, "tubeless")
+
+class frames():
+	def __init__(self,weight,cost,material):
+		self.weight = weight
+		self.cost = cost
+		self.material = material
+
+alum = frames(3,5,"aluminunum")   # Types of Frames
+carb = frames(2,6,"carbon")
+steel_frame = frames(1.50, 4.50, "steel")
+
+
+class models():
+
+class manufacturers():
+	
+
+class shops()
+
+
+
+class customers()
+
