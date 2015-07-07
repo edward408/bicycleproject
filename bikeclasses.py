566292d7baf987d57b3e6c24355cf4428ff1746b
edward408 <edrodriguezrobles@gmail.com>
2015-07-07 11:02:29 -0500

    Update bikeclasses.py

--- bikeclasses.py
+++ bikeclasses.py
@@ -14,27 +14,41 @@ class Bike_Shop(object):
     """Defines the bike shop with its respective inventory"""
     def __init__(self,store_name):
         self.store_name = store_name
-        self.bike_inventory = {}
+        self.store_inventory = []
+        self.affordable_bikes = []
         
 
     def display_bikes(self):
-        print "The store is called %s and it has %s" % (self.store_name,self.bike_inventory)
+        print "The store is called %s and it has %s" % (self.store_name,self.bikes)
+
+    def bikes_under(self, budget):
+        for bike in self.store_inventory:
+            if bike.retail_cost <= budget:
+                self.affordable_bikes.append(bike)
+
+    def getAffordableBikes(self):
+        return self.affordable_bikes
         
         
-    
-class Customers(object):
+        
+class Customers(Bike_Shop):
     
     def __init__(self,name,budget):
         self.name = name
         self.budget = budget
         self.shopping_cart ={}
 
-    def purchase(self):
-        pass 
+    def purchase(self,store_inventory,affordable):
 
+        for bike in store_inventory:
+            if bike in affordable:
+                self.shopping_cart[bike.model]=bike.retail_cost
+                self.budget -=bike.retail_cost
+                store_inventory.remove(bike)
+                
+        
     def display_customers(self):
         print "Customer is %s and with a budget of %s dollars" % (self.name, self.budget)
-    
 
 
 
