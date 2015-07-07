a23f624458ae91eed1bbfe513e4819e659c5296a
edward408 <edrodriguezrobles@gmail.com>
2015-07-07 11:01:17 -0500

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from bikes import *
 
 
-
-customer_1 = Customers("Jeff",50)
-customer_2 = Customers("Ryan",100)
-customer_3 = Customers("John",30)
-cust_list = [customer_1,customer_2,customer_3]
-def customers_walkin():
-    for customers in cust_list:
-        customers.display_customers()
-        
-customers_walkin()
-
 def main():
+    
+    #Customer instances
+    customer_1 = Customers("Jeff",50)
+    customer_2 = Customers("Ryan",100)
+    customer_3 = Customers("John",30)
+    cust_list = [customer_1,customer_2,customer_3]
+    def customers_walkin():
+        for customers in cust_list:
+            customers.display_customers()
+        
+    customers_walkin()
 
+    #Bicycle instances
     bike_1 = Bicycle("Xtreme",5, 20)
     bike_2 = Bicycle("Passive",5, 15)
     bike_3 = Bicycle("EcoRide", 5, 18)
@@ -22,28 +23,31 @@ def main():
     bike_6 = Bicycle("Kidsgo",3, 10)
     bike_list = [bike_1, bike_2, bike_3, bike_4, bike_5, bike_6]
 
+    #Shows the inventory of Bobs Bike Shop
     store = Bike_Shop("Bobs Bike Shop")
     for bikes in bike_list:
-        store.bike_inventory[bikes.model]=bikes.retail_cost
+        store.store_inventory.append(bikes)
 
-    print (store.bike_inventory)
+    print "This is the available shop inventory"    
+    print store.store_inventory
+
 
     for customers in cust_list:
-        for bikes in bike_list:
-            if customers.budget > bikes.retail_cost:
-                print customers.name + " " + "Can Afford", bikes.model
-                customers.shopping_cart[bikes.model]= bikes.retail_cost
-                customers.budget -= bikes.retail_cost
-                print customers.budget
-        
-        
+            print customers.name
+            print store.bikes_under(customers.budget)
+            customers.purchase(store.store_inventory,store.bikes_under(customers.budget))
+
+    print "This is the remaining shop inventory"
+    print store.store_inventory
+            
 
     
-
+        
+            
+    
 
 if __name__ == '__main__':
     main()
-
 
 
 
